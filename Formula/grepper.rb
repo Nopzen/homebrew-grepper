a0f9a30f767d4f155bf042a7cb398fa179cfb43d
Lars Krieger <lars.krieger@gmail.com>
2019-05-15 16:43:30 +0200

    update: use v1.0.1

--- Formula/grepper.rb
+++ Formula/grepper.rb
@@ -1,13 +1,13 @@
 class Grepper < Formula
   desc "Recursive grep function for the command line"
   homepage ""
-  url "https://github.com/Nopzen/grepper/archive/v1.0.0.tar.gz"
-  sha256 "98c76a536d8805f09d75ae280e13ca0f50cf8bdace84d83add123c03af4caa1a"
+  url "https://github.com/Nopzen/grepper/archive/v1.0.1.tar.gz"
+  sha256 "a06ae4568cb6a3c40850366101e211cd087cb50c21d861552c654115efc73529"
   
   depends_on "crystal" => :build
 
   def install
-    system "crystal build src/main.cr -o grepper"
-    bin.install "grepper"
+    system "make"
+    bin.install "./bin/grepper"
   end
 end
